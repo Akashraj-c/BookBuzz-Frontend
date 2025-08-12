a65e1bb418df5ef753a68fc119ec1ecb3bf3c1a1
Akashraj-c <akashrajc31@gmail.com>
2025-08-13 00:40:55 +0530

    BookBuzz v3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AiOutlineLogin } from "react-icons/ai";
+import { RiLogoutCircleLine } from "react-icons/ri";
 
 const Header = () => {
+
+    useEffect(() => {
+        if (sessionStorage.getItem('token')) {
+            const tok = sessionStorage.getItem('token')
+            setToken(tok)
+        }
+    }, [])
+    const navigate = useNavigate()
+
+    const [token, setToken] = useState('')
+    // console.log(token);
+
+    // handle Logout
+    const handleLogout = async () => {
+        sessionStorage.removeItem('existingUser')
+        sessionStorage.removeItem('token')
+        navigate('/login')
+    }
+
     return (
         <>
             <nav>
                 <div className="container-fluid shadow" >
                     <div className="row  d-flex align-items-center px-4">
                         <div className="col-md-2 d-flex justify-content-start">
-                           <Link to={'/'}> <img src="https://static.vecteezy.com/system/resources/previews/024/043/963/original/book-icon-clipart-transparent-background-free-png.png" alt="no img" style={{ width: '110px' }} /></Link>
+                            <Link to={'/'}> <img src="https://static.vecteezy.com/system/resources/previews/024/043/963/original/book-icon-clipart-transparent-background-free-png.png" alt="no img" style={{ width: '110px' }} /></Link>
                         </div>
                         <div className="col-md-8 d-flex justify-content-center">
                             <h2 className='fw-bold' style={{ userSelect: 'none' }}>BOOKBUZZ</h2>
                         </div>
                         <div className="col-md-2 d-flex justify-content-end">
-                            <button className='btn border border-black w-50'>login</button>
+                            {!token ? <Link to={'/login'}><button className='btn border border-black'>Login <AiOutlineLogin className='text-success fs-4 ms-2' /></button></Link>
+                                :
+                                <button onClick={handleLogout} className='btn border border-black '>Logout <RiLogoutCircleLine className='text-danger fs-4 ms-2' /></button>}
                         </div>
                     </div>
                 </div>
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,20 +1,95 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
-import TextField from '@mui/material/TextField';
+import { loginApi, registerApi } from '../services/allApi';
+import { toast, ToastContainer } from 'react-toastify';
 
 const Auth = ({ register }) => {
+    const navigate = useNavigate()
+
     const [passwordEye, setPAsswordEye] = useState(false)
     const [userDetails, setUserDetails] = useState({
         username: "",
         email: "",
         password: ""
     })
-    console.log(userDetails);
+    // console.log(userDetails);
 
-    const handleRegister = () => {
-        
+    // User Registration
+    const handleRegister = async () => {
+        const { username, email, password } = userDetails
 
+        if (!username || !email || !password) {
+            toast.error('please fill the form completely')
+        }
+        else {
+            const result = await registerApi({ username, email, password })
+            console.log(result);
+
+            if (result.status == 200) {
+                toast.success('Registered Succesfully')
+                setUserDetails({
+                    username: "",
+                    email: "",
+                    password: ""
+                })
+                navigate('/login')
+            }
+            else if (result.status == 402) {
+                toast.info(result.response.data)
+            }
+            else {
+                toast.error('Something went wrong')
+            }
+        }
+    }
+
+    // User Login
+    const handleLogin = async () => {
+        const { email, password } = userDetails
+
+        if (!email || !password) {
+            toast.error('Please fill the form completely')
+        }
+        else {
+            const result = await loginApi({ email, password })
+            console.log(result);
+
+            if (result.status == 200) {
+                toast.success('login successfully')
+                setTimeout(() => {
+                    navigate('/')
+                }, 2000)
+                setUserDetails({
+                    username: "",
+                    email: "",
+                    password: ""
+                })
+
+                sessionStorage.setItem('existingUser', JSON.stringify(result.data.existingUser))
+                sessionStorage.setItem('token', result.data.token)
+
+            }
+            else if (result.status == 409) {
+                toast.error(result.response.data)
+                setUserDetails({
+                    username: "",
+                    email: "",
+                    password: ""
+                })
+            }
+            else if (result.status == 403) {
+                toast.error(result.response.data)
+                setUserDetails({
+                    username: "",
+                    email: "",
+                    password: ""
+                })
+            }
+            else {
+                toast.error('Something went wrong')
+            }
+        }
     }
 
     return (
@@ -52,13 +127,13 @@ const Auth = ({ register }) => {
                             </div>
 
                             {!register ? <div className='w-100'>
-                                <button className='btn btn-success w-100'>Login</button>
+                                <button type='button' onClick={handleLogin} className='btn btn-success w-100'>Login</button>
 
                                 <p className='text-white mt-2 text-center' style={{ fontSize: '15px' }}>Are you a New User ? <Link to={'/register'}>Register</Link></p>
                             </div>
                                 :
                                 <div className='w-100'>
-                                    <button className='btn btn-success w-100' onClick={handleRegister}>Register</button>
+                                    <button className='btn btn-success w-100' type='button' onClick={handleRegister}>Register</button>
 
                                     <p className='text-white mt-2 text-center' style={{ fontSize: '15px' }}>Are you Already a User ? <Link to={'/login'}>Login</Link></p>
                                 </div>}
@@ -68,8 +143,7 @@ const Auth = ({ register }) => {
                     <div className="col-md-3"></div>
                 </div>
             </div>
-
-
+            <ToastContainer position='top-center' autoClose={2000} theme='colored' />
         </>
     )
 }
